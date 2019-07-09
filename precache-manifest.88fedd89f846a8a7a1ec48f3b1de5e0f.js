88d1955cef2e25d847b86ac765ce7c7950bd5578
Washington-Ksea <ksea.washington@gmail.com>
2019-07-09 22:25:37 +0900

    Updates

---
+++ precache-manifest.88fedd89f846a8a7a1ec48f3b1de5e0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f26d1f5bce08162e53b2767d361a2d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dde55a9be2abdf93522e",
+    "url": "/monster-rolodex/static/css/main.8006036e.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/monster-rolodex/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "dde55a9be2abdf93522e",
+    "url": "/monster-rolodex/static/js/main.45320fbb.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
